426f7d6969fd2221b92625c30131d0c22b874f59
Sybsuper <sybsuper@gmail.com>
2026-08-06 19:10:38 +0200

    feat: add command argument names to teleport command

--- src/main/kotlin/commands/Teleport.kt
+++ src/main/kotlin/commands/Teleport.kt
@@ -3,10 +3,11 @@ package com.sybsuper.terradevserver.commands
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.Player
 import revxrsal.commands.annotation.Command
+import revxrsal.commands.annotation.Named
 
 class Teleport : ICommand {
     @Command("teleport", "tp")
-    fun teleport(sender: Player, x: Double, y: Double, z: Double) {
+    fun teleport(sender: Player, @Named("x") x: Double, @Named("y") y: Double, @Named("z") z: Double) {
         val location = Pos(x, y, z)
         sender.teleport(location)
     }
